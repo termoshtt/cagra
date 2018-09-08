36b38bf1c3c494b8ff825a2be33f03f5827c08ae
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-09-09 02:05:01 +0900

    Fromat

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,8 @@
-
 pub type Result<T> = ::std::result::Result<T, Error>;
 
 #[derive(Fail, Debug)]
 pub enum Error {
     /// node type mismatch
     #[fail(display = "Node type mismatch (Index = {})", index)]
-    NodeTypeError {
-        index: usize
-    },
+    NodeTypeError { index: usize },
 }
