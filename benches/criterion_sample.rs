790c6e18d83bc40a862fe3779e84c207f6b78985
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-11-17 20:21:43 +0900

    Criterion Getting started

---
+++ benches/criterion_sample.rs
@@ -0,0 +1,19 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fib 20", |b| b.iter(|| fibonacci(20)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
